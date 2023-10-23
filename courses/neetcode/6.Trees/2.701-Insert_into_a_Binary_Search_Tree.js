e9e6e68ba90e31ee448bf527679c0c60c5503be0
Wesley Moura <wesleytmoura@gmail.com>
2023-10-23 19:50:47 -0300

    docs: solving Insert into a BST exercise

---
+++ courses/neetcode/6.Trees/2.701-Insert_into_a_Binary_Search_Tree.js
@@ -0,0 +1,30 @@
+/**
+ * 701. Insert into a Binary Search Tree
+ * https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+ */
+
+ /**
+  Definition for a binary tree node. */
+  function TreeNode(val, left, right) {
+      this.val = (val===undefined ? 0 : val)
+      this.left = (left===undefined ? null : left)
+      this.right = (right===undefined ? null : right)
+  }
+ 
+  /*
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+
+var insertIntoBST = function(root, val) {
+  if (root == null) return new TreeNode(val)
+
+  if (val > root.val) {
+      root.right = insertIntoBST(root.right, val)
+  } else if (val < root.val) {
+      root.left = insertIntoBST(root.left, val)
+  }
+
+  return root
+};
