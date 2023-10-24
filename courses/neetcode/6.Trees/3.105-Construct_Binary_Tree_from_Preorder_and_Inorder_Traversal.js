52ee861ba1f6acfb0250eda4e6e0815e90b2f48c
Wesley Moura <wesleytmoura@gmail.com>
2023-10-24 09:34:45 -0300

    docs: Adding exercise construct binary tree

---
+++ courses/neetcode/6.Trees/3.105-Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.js
@@ -0,0 +1,31 @@
+/**
+ * 105. Construct Binary Tree from Preorder and Inorder Traversal
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    if (preorder.length === 0 || inorder.length === 0) {
+        return null;
+    }
+
+    const root = new TreeNode(preorder[0]);
+    const mid = inorder.indexOf(preorder[0]);
+
+    root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+    root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+
+    return root;
+};
